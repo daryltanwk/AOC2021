5fc8c5cfe4de9fb43aae096de1e2c6c2c6fa8f6b
Daryl Tan <daryltanwk@gmail.com>
2021-12-01 21:06:51 +0800

    Day 1: Puzzle 1

--- Day_1/aoc2021_1.py
+++ Day_1/aoc2021_1.py
@@ -1,1 +1,19 @@
-print('Hello World')
+# Open file for reading
+inputFile = open('input_1', 'rt')
+
+# Put values into array
+inputValues = []
+for x in inputFile:
+    inputValues.append(int(x))
+
+inputFile.close()
+"""
+PUZZLE ONE
+"""
+increaseCount = 0
+
+for currentIndex in range(len(inputValues)-1):
+    if inputValues[currentIndex+1]-inputValues[currentIndex] > 0:
+        increaseCount += 1
+
+print("Increase Count: ", increaseCount)
